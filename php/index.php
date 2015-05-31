9ecc5667bb9ab0231933ad9a11f2f14a51d7959e
emeric254 <emeric254@live.fr>
2015-05-31 23:04:28 +0200

    maj

--- php/index.php
+++ php/index.php
@@ -1,6 +1,30 @@
 <?php
+    session_start();
+
+    $path = "pages/erreur.php";
+
+    if ( isset($_GET['page']) and !empty($_GET['page']) )
+    {
+        $page = htmlspecialchars($_GET['page']);
+
+        if ( file_exists("core/$page.php") )
+        {
+            $path = "core/$page.php";
+        }
+        else
+        {
+            $errMsg = "page introuvable";
+        }
+    }
+    else
+    {
+        $page="accueil"; //page par default, l'accueil
+    }
+
     include("static/haut.php");
     include("static/navbar.php");
-    include("pages/accueil.php");
+
+    include($path);
+
     include("static/bas.php");
 ?>
