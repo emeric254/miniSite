0ea69a3bb19c808ddf0ff9f2b70a6454c97d6b29
emeric254 <emeric254@live.fr>
2015-05-31 23:13:16 +0200

    maj

--- php/static/haut.php
+++ php/static/haut.php
@@ -9,14 +9,11 @@
             TITRE
         </title>
         <link rel="icon" href="favicon.ico">
-        <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-        <!-- Font-Awesome -->
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-        <!-- JQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     </head>
     <body role="document" style="padding-top: 50px; padding-bottom: 20px; margin-bottom: 60px;">
 
