8aab7cc9132dad471f5526898ab0e1dec45063fa
emeric254 <emeric254@live.fr>
2015-05-31 23:17:24 +0200

    maj

--- php/static/bas.php
+++ php/static/bas.php
@@ -1,4 +1,4 @@
-        <footer class="footer" style="position: absolute; bottom: 0; width: 100%; height: 60px; background-color: #f5f5f5;">
+        <footer class="footer" style="position: absolute; bottom: 0; width: 100%; height: 30px; background-color: #f5f5f5;">
             <div class="container">
                 <p class="pull-right">
                     <span class="label label-info">
--- php/static/haut.php
+++ php/static/haut.php
@@ -15,4 +15,4 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     </head>
-    <body role="document" style="padding-top: 50px; padding-bottom: 20px; margin-bottom: 60px;">
+    <body role="document" style="padding-top: 50px; padding-bottom: 20px; margin-bottom: 30px;">
